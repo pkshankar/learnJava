a78fd0678e0c2ee1b42000ff5908340382566a23
pkshankar <shankarpk89@gmail.com>
2020-04-10 19:17:51 +0530

    Adding streams print even

--- src/learnJava8/PrintEven.java
+++ src/learnJava8/PrintEven.java
@@ -8,14 +8,25 @@ public class PrintEven {
 	public static void main(String[] args) {
 
 		List<Integer> numList = Arrays.asList(1, 2, 3, 4, 5, 94, 38, 90, -24, 24);
-		numList.forEach(i -> {
 
-			if (i > 1 && i % 2 == 0) {
+		// USING LAMBDA EXPRESSIONS
 
-				System.out.println(i);
-			}
+		System.out.println("USING LAMBDA EXPRESSIONS");
+		 numList.forEach(i -> {
+		
+		 if (i > 1 && i % 2 == 0) {
+		
+		 System.out.println(i);
+		 }
+		
+		 });
 
-		});
+		// USING STREAMS
+
+		System.out.println("*****************************");
+		System.out.println("USING STREAMS");
+
+		numList.stream().filter(i -> i % 2 == 0 && i > 1).forEach(j -> System.out.println(j));
 
 	}
 
